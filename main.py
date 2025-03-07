ee8f04f4b3f7184ff22624cac133388b2fcd02f0
Jason2Car <91041525+Jason2Car@users.noreply.github.com>
2025-03-07 18:02:07 +0000

    added single line file

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ text=""
 with open('Dalloway.txt', encoding='utf8') as file_object:
     text = file_object.read()
 
-for dalloway in text.split():
-    print(dalloway)
+text = text.split()
+name = 0;
+title = 0;
+for index in range(len(text)-1):
+    if text[index] == "Mrs." and text[index+1] == "Dalloway":
+        title+=1
+    elif text[index] == "Clarissa":
+        name+=1
+print("Mrs. Dalloway mentioned: "+str(title))
+print("Clarissa mentioned: "+ str(name))
